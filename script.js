53f4128539e90afd763ac3ab30fb3bcf28083845
Lydos <lydossky@gmail.com>
2024-10-14 20:50:44 +0300

    end of project

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector(".grid-size-btn");
 
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  div.classList.add("square", `${i + 1}`);
-  container.appendChild(div);
-  div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = "#0b192c";
-  });
-}
+const btnEventHandler = function () {
+  const input = parseInt(prompt("Please enter a grid size "));
+
+  if (Number.isInteger(input)) {
+    container.innerHTML = null;
+    gridCreater(input);
+  } else {
+    alert("C'mon Now ");
+  }
+};
+
+btn.addEventListener("click", btnEventHandler);
+
+const gridCreater = function (size) {
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square", `${i + 1}`);
+    div.style.flexBasis = `${100 / size}%`;
+    div.style.height = `${100 / size}%`;
+    container.appendChild(div);
+    div.addEventListener("mouseenter", () => {
+      div.style.backgroundColor = "#ab886d";
+    });
+  }
+};
+
+gridCreater(4);
